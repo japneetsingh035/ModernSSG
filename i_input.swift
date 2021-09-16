d2b32deb896b3a09a9a85239777740030305a567
Japneet Singh <47236363+japneetsingh035@users.noreply.github.com>
2021-09-16 03:00:52 -0400

    Add files via upload

---
+++ i_input.swift
@@ -0,0 +1,62 @@
+//
+//  i_input.swift
+//  
+//
+//  Created by Japneet Kalra on 2021-09-15.
+//
+
+import Foundation
+import ArgumentParser
+
+extension Command {
+  struct i_input: ParsableCommand{
+    static var configuration: CommandConfiguration {
+          .init(
+            commandName: "i",
+            abstract: "allow the user to specify an input directory to be processed"
+          )
+        }
+    @Argument(help: "convert this text file to html")
+    var folder_name : String
+    var new_result = " ";
+    mutating func run() throws{
+        //defining the url of folder
+        let url = URL(fileURLWithPath: NSHomeDirectory() + "/Desktop/ModernSSG/" + self.folder_name)
+        var files = [URL]()
+        //
+        if let enumerator = FileManager.default.enumerator(at: url, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
+            for case let fileURL as URL in enumerator {
+                do {
+                    let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
+                    if fileAttributes.isRegularFile! {
+                        new_result = try String(contentsOf: fileURL, encoding: .utf8)
+                        new_result = new_result.replacingOccurrences(of: "\n\n" , with: "<p>", options: .regularExpression)
+                        files.append(fileURL)
+                    }
+                } catch { print(error, fileURL) }
+            }
+        }
+        //
+        for i in 0..<files.count {
+        let fileName = files[i].deletingPathExtension().lastPathComponent
+        print(fileName);
+        do {
+            new_result = try String(contentsOf: files[i], encoding: .utf8)
+            new_result = new_result.replacingOccurrences(of: "\n\n" , with: "<p>", options: .regularExpression)
+            new_result = new_result.replacingOccurrences(of: fileName, with: "")
+        }catch {print("Error in reading data")}
+        let storedText =  "<!doctype html><html><head><meta charset='utf-8'><title>" + fileName + "</title><meta name='viewport' content='width=device-width', initial-scale=1'></head><body><h1>" + fileName + "</h1>" + new_result + "</body></html>";
+        let filePath = NSHomeDirectory() + "/Desktop/ModernSSG/Dist/" + fileName + ".HTML"
+            if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
+                {
+                    print("File Created")
+                    try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
+                }
+            else{
+                print("Error in writing data")
+            }
+        }
+       }
+    }
+  }
+
---
+++ main.swift
@@ -0,0 +1,23 @@
+import ArgumentParser
+
+enum Command {}
+
+extension Command {
+  struct Main: ParsableCommand {
+    static var configuration: CommandConfiguration {
+      .init(
+        commandName: "ModernSSG",
+        abstract: "A program to convert text files to HTML site generator",
+        version: "0.0.1",
+        subcommands: [
+            Command.input.self,
+            Command.i_input.self,
+            Command.Output.self,
+            Command.stylesheet.self
+        ]
+      )
+    }
+  }
+}
+
+Command.Main.main()
