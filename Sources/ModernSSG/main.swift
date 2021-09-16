f32e0035ff2c2787a9e532da418e473f4e4515b1
Japneet Singh <47236363+japneetsingh035@users.noreply.github.com>
2021-09-16 16:08:44 -0400

    Update main.swift

--- Sources/ModernSSG/main.swift
+++ Sources/ModernSSG/main.swift
@@ -8,7 +8,7 @@ extension Command {
       .init(
         commandName: "ModernSSG",
         abstract: "A program to convert text files to HTML site generator",
-        version: "0.0.1",
+        version: "Modern SSG 0.0.1",
         subcommands: [
             Command.input.self,
             Command.i_input.self,
