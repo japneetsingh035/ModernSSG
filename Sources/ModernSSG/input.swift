d13a26eab08b21d76fd20ebd84e0b459ee795aec
Japneet Singh <47236363+japneetsingh035@users.noreply.github.com>
2021-09-17 01:06:46 -0400

    Add files via upload

--- Sources/ModernSSG/input.swift
+++ Sources/ModernSSG/input.swift
@@ -4,42 +4,46 @@
 //
 //  Created by Japneet Kalra on 2021-09-14.
 //
-
 import Foundation
 import ArgumentParser
 
 extension Command {
-  struct input: ParsableCommand {
-    static var configuration: CommandConfiguration {
-          .init(
-            commandName: "input",
-            abstract: "allow the user to specify an input file to be processed"
-          )
+    struct input: ParsableCommand {
+        static var configuration: CommandConfiguration {
+            .init(
+                commandName: "input",
+                abstract: "allow the user to specify an input file to be processed",
+                subcommands: [Command.Output.self, Command.stylesheet.self]
+            )
         }
-    @Argument(help: "convert this text file to html")
-    var filename: String
-    var result = ""
-    mutating func run() throws{
-        
-        //Searching the file in desktop directory
-        if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
-            let fileURL = dir.appendingPathComponent(self.filename)
+        @Argument(help: "convert this text file to html")
+        var filename: String
+        var result = ""
+        mutating func run() throws{
+            
+            //Searching the file in desktop directory
+            
+            let fileURL = URL(fileURLWithPath: NSHomeDirectory() + "/Desktop/ModernSSG/" + self.filename)
             let fName: NSString = self.filename as NSString
             let pathPrefix = fName.deletingPathExtension
             print("File URL to read \(fileURL)")
             
             //Reading the data
+            
             do {
                 result = try String(contentsOf: fileURL, encoding: .utf8)
-                result = result.replacingOccurrences(of: "\n\n" , with: "<p>", options: .regularExpression)
+                result = result.replacingOccurrences(of: "\n\n\n", with: "<p>", options: .regularExpression)
+                result = result.replacingOccurrences(of: "\n\n", with: "</p><p>", options: .regularExpression)
                 result = result.replacingOccurrences(of: pathPrefix, with: "")
                 print("File data copied")
             }
             catch {print("Error in reading file. Please store text in desktop directory")}
             
             //Writing the data
-            let storedText =  "<!doctype html><html><head><meta charset='utf-8'><title>" + pathPrefix + "</title><meta name='viewport' content='width=device-width', initial-scale=1'></head><body><h1>" + pathPrefix + "</h1>" + result + "</body></html>";
-            let filePath = NSHomeDirectory() + "/Desktop/" + pathPrefix + ".html"
+            
+            let storedText =  "<!doctype html><html><head><meta charset='utf-8'><title>\(pathPrefix)</title><meta name='viewport' content='width=device-width, initial-scale=1'></head><body><h1>\(pathPrefix)</h1>\(result)</body></html>";
+            let filePath = NSHomeDirectory() + "/Desktop/ModernSSG/Dist/" + pathPrefix + ".html"
+            
             if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
             {
                 try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
@@ -50,5 +54,4 @@ extension Command {
             }
         }
     }
-  }
 }
