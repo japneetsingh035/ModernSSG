2ab346b2fe7a2aa936f508321578e734827131cc
Japneet Singh <47236363+japneetsingh035@users.noreply.github.com>
2021-09-17 22:11:39 -0400

    Add files via upload

--- Sources/ModernSSG/main.swift
+++ Sources/ModernSSG/main.swift
@@ -3,21 +3,21 @@ import ArgumentParser
 enum Command {}
 
 extension Command {
-  struct Main: ParsableCommand {
-    static var configuration: CommandConfiguration {
-      .init(
-        commandName: "ModernSSG",
-        abstract: "A program to convert text files to HTML site generator",
-        version: "Modern SSG 0.0.1",
-        subcommands: [
-            Command.input.self,
-            Command.i_input.self,
-            Command.Output.self,
-            Command.stylesheet.self
-        ]
-      )
+    struct Main: ParsableCommand {
+        static var configuration: CommandConfiguration {
+            .init(
+                commandName: "ModernSSG",
+                abstract: "A program to convert text files to HTML site generator",
+                version: "Modern SSG 0.0.1",
+                subcommands: [
+                    Command.input.self,
+                    Command.i_input.self,
+                    Command.Output.self,
+                    Command.stylesheet.self,
+                ]
+            )
+        }
     }
-  }
 }
 
 Command.Main.main()
