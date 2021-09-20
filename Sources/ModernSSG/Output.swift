8c396a2cce1281a0b2d0a4dd6b57684f90515240
Japneet Singh <47236363+japneetsingh035@users.noreply.github.com>
2021-09-20 19:38:33 -0400

    Add files via upload

--- Sources/ModernSSG/Output.swift
+++ Sources/ModernSSG/Output.swift
@@ -8,51 +8,14 @@ import Foundation
 import ArgumentParser
 
 extension Command {
-  struct Output: ParsableCommand {
-    static var configuration: CommandConfiguration {
-          .init(
-            commandName: "output",
-            abstract: "allow the user to specify an output folder to be processed"
-          )
+    struct Output: ParsableCommand {
+        static var configuration: CommandConfiguration {
+            .init(
+                commandName: "--output",
+                abstract: "allow the user to specify an --output or -o folder to be processed"
+            )
         }
-    @Argument(help: "all html files moved to destination folder")
-    var folder_name: String
-    var result = "";
-    mutating func run() throws {
-        let url = URL(fileURLWithPath: NSHomeDirectory() + "/Desktop/ModernSSG/Sherlock-Holmes-Selected-Stories/")
-        var files = [URL]()
-        if let enumerator = FileManager.default.enumerator(at: url, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
-            for case let fileURL as URL in enumerator {
-                do {
-                    let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
-                    if fileAttributes.isRegularFile! {
-                        result = try String(contentsOf: fileURL, encoding: .utf8)
-                        files.append(fileURL)
-                    }
-                } catch { print(error, fileURL) }
-            }
-        }
-        for i in 0..<files.count {
-        let fileName = files[i].deletingPathExtension().lastPathComponent
-        print(fileName);
-        do {
-            result = try String(contentsOf: files[i], encoding: .utf8)
-            result = result.replacingOccurrences(of: "\n\n" , with: "<p>", options: .regularExpression)
-            result = result.replacingOccurrences(of: fileName, with: "")
-        }
-        catch {
-            print("Error in reading data")
-        }
-        let storedText =  "<!doctype html><html><head><meta charset='utf-8'><title>" + fileName + "</title><meta name='viewport' content='width=device-width', initial-scale=1'></head><body><h1>" + fileName + "</h1>" + result + "</body></html>";
-        let filePath = NSHomeDirectory() + "/Desktop/ModernSSG/" + self.folder_name + "/" + fileName + ".HTML"
-        if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
-            {
-                try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
-            }
-        else{
-                print("the specified output path is not a valid directory. Please add directory to same folder.")
-            }
-        }
+        @Argument(help: "all html files moved to destination folder")
+        var output: String
     }
-  }
 }
--- Sources/ModernSSG/input.swift
+++ Sources/ModernSSG/input.swift
@@ -1,9 +1,10 @@
 //
 //  input.swift
-//  
+//
 //
 //  Created by Japneet Kalra on 2021-09-14.
 //
+
 import Foundation
 import ArgumentParser
 
@@ -16,40 +17,57 @@ extension Command {
                 subcommands: [Command.Output.self, Command.stylesheet.self]
             )
         }
-        @Argument(help: "convert this text file to html")
-        var filename: String
+        @Option(name: .shortAndLong, help: "Enter the stylesheet URL to be added in body tags.") var stylesheet: String?
+        
+        @Option(name: .long, help: "Enter the output folder to move html file") var output: String?
+
+        @Argument(help: "convert this text file to html") var filename: String
         var result = ""
         mutating func run() throws{
-            
             //Searching the file in desktop directory
             
             let fileURL = URL(fileURLWithPath: NSHomeDirectory() + "/Desktop/ModernSSG/" + self.filename)
             let fName: NSString = self.filename as NSString
             let pathPrefix = fName.deletingPathExtension
             print("File URL to read \(fileURL)")
-            
-            //Reading the data
+
             do {
+                //Reading the data
+                //!!!!!!!!!!!!!!!!
                 result = try String(contentsOf: fileURL, encoding: .utf8)
-                result = result.replacingOccurrences(of: "\n\n\n", with: "<p>", options: .regularExpression)
-                result = result.replacingOccurrences(of: "\n\n", with: "</p><p>", options: .regularExpression)
-                result = result.replacingOccurrences(of: pathPrefix, with: "", options: .regularExpression)
+                result = result.replacingOccurrences(of: "\r?\n\r?\n\r?\n", with: "<p>", options: .regularExpression)
+                result = result.replacingOccurrences(of: "\r?\n\r?\n", with: "</p><p>", options: .regularExpression)
+                result = result.replacingOccurrences(of: pathPrefix, with: " ", options: .regularExpression)
                 print("File data copied")
+                
+                //Writing the data
+                //!!!!!!!!!!!!!!!!
+                let storedText =  "<!doctype html><html><head><LINK rel='stylesheet' href=\(self.stylesheet ?? " ")><meta charset='utf-8'><title>\(pathPrefix)</title><meta name='viewport' content='width=device-width, initial-scale=1'></head><body><h1>\(pathPrefix)</h1>\(result)</body></html>";
+                
+                if (self.output != nil) {
+                    let filePath = NSHomeDirectory() + "/Desktop/ModernSSG/\(self.output ?? "" )/\(pathPrefix).html"
+                    if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
+                    {
+                        try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
+                        print("File Created at \(filePath)")
+                    }
+                    else{
+                        print("Error in writing data");
+                    }
+                }
+                else{
+                    let filePath = NSHomeDirectory() + "/Desktop/ModernSSG/Dist/\(pathPrefix).html"
+                    if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
+                    {
+                        try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
+                        print("File Created at \(filePath)")
+                    }
+                    else{
+                        print("Error in writing data");
+                    }
+                }
             }
-            catch {print("Error in reading file. Please store text in desktop directory")}
-            
-            //Writing the data
-            let storedText =  "<!doctype html><html><head><meta charset='utf-8'><title>\(pathPrefix)</title><meta name='viewport' content='width=device-width, initial-scale=1'></head><body><h1>\(pathPrefix)</h1>\(result)</body></html>";
-            let filePath = NSHomeDirectory() + "/Desktop/ModernSSG/Dist/\(pathPrefix).html"
-            
-            if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
-            {
-                try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
-                print("File Created at \(filePath)")
-            }
-            else{
-                print("error in writing data");
-            }
+            catch {print("Error in reading file. Please check name and extension of file.                                           *** File must be in current directory ***")}
         }
     }
 }
