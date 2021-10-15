2271269d6cd8a0f28e2f474a7155ea3b104fa477
Japneet Singh <japneetsingh03.js@gmail.com>
2021-10-15 01:01:54 -0400

    Refactoring main.swift to improve code maintainability:
    * Improves Indendation & comments
    * Removed Global Variables
    * Refracting of functions
    * Rename some variables to be more clear
    * commit temporary files

--- Sources/ModernSSG/main.swift
+++ Sources/ModernSSG/main.swift
@@ -9,15 +9,12 @@ extension Command {
             .init(
                 commandName: "ModernSSG",
                 abstract: "A program to convert text files to HTML site generator",
-                version: "Modern SSG 0.0.1"
+                version: "ModernSSG 0.0.1"
             )
         }
         
         //input
         @Option(name: .shortAndLong ,help:"allow the user to specify an input file or folder to be processed") var input: String
-        
-        //config
-        //@Option(name: .shortAndLong ,help:"allow the user to specify an input file or folder to be processed") var config: String?
         
         //stylesheet
         @Option(name: .shortAndLong, help: "allow the user to optionally specify a URL to a CSS stylesheet.") var stylesheet: String?
@@ -28,132 +25,137 @@ extension Command {
         //lang
         @Option(name: .shortAndLong, help:"allow the user to specify the language attribute on root html.") var lang: String?
         
-        
-        var result = ""
-        var new_result = "";
         mutating func run() throws{
-            //Searching the file in desktop directory
+            
+            //Searching the argumented file in current directory
             if (self.input.hasSuffix(".txt")||self.input.hasSuffix(".md")){
-                    let fileURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath + "/" + self.input)
-                    let fName: NSString = self.input as NSString
-                    let pathPrefix = fName.deletingPathExtension
-                    print("File URL to read \(fileURL)")
+                filereadAndWrite()
+            }
+            //Searching the argumented folder in current directory
+            else{
+                readAndWriteFolder()
+            }
+        }
+        func filereadAndWrite() {
+            do {
+                //Reading & Copying the data
+                //!!!!!!!!!!!!!!!!
+                let fileURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath + "/" + self.input)
+                let fileName: NSString = self.input as NSString
+                let pathPrefix = fileName.deletingPathExtension
+                print("File URL to read \(fileURL)")
+                var data = try String(contentsOf: fileURL, encoding: .utf8)
+                //conversion of text to html
+                if(self.input.hasSuffix(".txt")) {
+                    data = data.replacingOccurrences(of: "\r?\n\r?\n\r?\n", with: "<p>", options: .regularExpression)
+                    data = data.replacingOccurrences(of: "\r?\n\r?\n", with: "</p><p>", options: .regularExpression)
+                    data = data.replacingOccurrences(of: "\(pathPrefix)", with: "", options: .regularExpression)
+
+                //conversion of MD to html
+                } else if(self.input.hasSuffix(".md")){
+                    data = data.replacingOccurrences(of: " _", with: "<i>", options: .regularExpression)
+                    data = data.replacingOccurrences(of: "_ ", with: "</i>", options: .regularExpression)
                     
-                    do {
-                        //Reading the data
-                        //!!!!!!!!!!!!!!!!
-                        result = try String(contentsOf: fileURL, encoding: .utf8)
-                        
-                        if(self.input.hasSuffix(".txt")) {
-                            result = result.replacingOccurrences(of: "\r?\n\r?\n\r?\n", with: "<p>", options: .regularExpression)
-                            result = result.replacingOccurrences(of: "\r?\n\r?\n", with: "</p><p>", options: .regularExpression)
-                            //MD support - italics
-                        } else if(self.input.hasSuffix(".md")){
-                            result = result.replacingOccurrences(of: " _", with: "<i>", options: .regularExpression)
-                            result = result.replacingOccurrences(of: "_ ", with: "</i>", options: .regularExpression)
-                            //Add support for inline <code> blocks //19
-                            result = result.replacingOccurrences(of: "\r?\n`", with: "<code>", options: .regularExpression)
-                            result = result.replacingOccurrences(of: "`\r?\n", with: "</code>", options: .regularExpression)
-                            //Add support for a horizontal rule in Markdown
-                            result = result.replacingOccurrences(of: "---", with: "<hr>", options: .regularExpression)
-                        }
-                        result = result.replacingOccurrences(of: pathPrefix, with: " ", options: .regularExpression)
-                        print("File data copied")
-                        
-                        //Writing the data
-                        //!!!!!!!!!!!!!!!!
-                        let storedText =
-                        "<!doctype html><html lang = \(self.lang ?? "en-CA")><head><LINK rel='stylesheet' href=\(self.stylesheet ?? " ")><meta charset='utf-8'><title>\(pathPrefix)</title><meta name='viewport' content='width=device-width, initial-scale=1'></head><body><h1>\(pathPrefix)</h1>\(result)</body></html>";
-                        
-                        if (self.output != nil) {
-                            let filePath = FileManager.default.currentDirectoryPath + "/ \(self.output ?? "" )/\(pathPrefix).html"
-                            if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
-                            {
-                                try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
-                                print("File Created at \(filePath)")
-                            }
-                            else{
-                                print("Error in writing data! Please create folder in current directory");
-                            }
-                        }
-                        else{
-                            let filePath = FileManager.default.currentDirectoryPath + "/Dist/\(pathPrefix).html"
-                            if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
-                            {
-                                try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
-                                print("File Created at \(filePath)")
-                            }
-                            else{
-                                print("Error in writing data! Please create dist folder if missing.");
-                                print("Error in writing data! Please create folder in current directory");
-                            }
-                        }
-                    }
-                    catch {print("Error in reading file. Please check name and extension of file.                                           *** File must be in current directory ***")}
+                    //Add support for inline <code> blocks //19
+                    data = data.replacingOccurrences(of: "\r?\n`", with: "<code>", options: .regularExpression)
+                    data = data.replacingOccurrences(of: "`\r?\n", with: "</code>", options: .regularExpression)
+                    
+                    //Add support for a horizontal rule in Markdown
+                    data = data.replacingOccurrences(of: "---", with: "<hr>", options: .regularExpression)
                 }
+                //Writing the data
+                //!!!!!!!!!!!!!!!!
+                let storedText =
+                    "<!doctype html><html lang = \(self.lang ?? "en-CA")><head><LINK rel='stylesheet' href=\(self.stylesheet ?? " ")><meta charset='utf-8'><title>\(pathPrefix)</title><meta name='viewport' content='width=device-width, initial-scale=1'></head><body><h1>\(pathPrefix)</h1>\(data)</body></html>";
                 
-                
+                //if output path is mentioned
+                if (self.output != nil) {
+                    let filePath = FileManager.default.currentDirectoryPath + "/ \(self.output ?? "" )/\(pathPrefix).html"
+                    if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
+                    {
+                        try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
+                        print("File Created at \(filePath)")
+                    }
+                    else{
+                        print("Error in writing data! Please create folder in current directory");
+                    }
+                }
+                //if output path is not mentioned
                 else{
-                    let url = URL(fileURLWithPath: FileManager.default.currentDirectoryPath + "/\(self.input)/")
-                    var files = [URL]()
-                    //Searching all text files in desktop directory
-                    if let enumerator = FileManager.default.enumerator(at: url, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) {                for case let fileURL as URL in enumerator {
-                        do {
-                            let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
-                            if fileAttributes.isRegularFile! {
-                                new_result = try String(contentsOf: fileURL, encoding: .utf8)
-                                new_result = new_result.replacingOccurrences(of: "\r?\n\r?\n\r?\n", with: "<p>", options: .regularExpression)
-                                new_result = new_result.replacingOccurrences(of: "\r?\n\r?\n", with: "</p><p>", options: .regularExpression)
-                                files.append(fileURL)
-                            }
-                        }catch { print(error, fileURL) }
+                    let filePath = FileManager.default.currentDirectoryPath + "/Dist/\(pathPrefix).html"
+                    if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
+                    {
+                        try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
+                        print("File Created at \(filePath)")
                     }
-                }
-                    //
-                    for i in 0..<files.count {
-                        let fileName = files[i].deletingPathExtension().lastPathComponent
-                        if (files[i].lastPathComponent.hasSuffix(".txt")){
-                            do {
-                                //Reading the data
-                                //!!!!!!!!!!!!!!!!
-                                new_result = try String(contentsOf: files[i], encoding: .utf8)
-                                new_result = new_result.replacingOccurrences(of: "\n\n\n", with: "<p>", options: .regularExpression)
-                                new_result = new_result.replacingOccurrences(of: "\n\n", with: "</p><p>", options: .regularExpression)
-                                new_result = new_result.replacingOccurrences(of: fileName, with: "", options: .regularExpression)
-                                print("File data copied")
-                                
-                                //Writing the data
-                                //!!!!!!!!!!!!!!!!
-                                let storedText =  "<!doctype html><html lang =\(self.lang ?? "en-CA")><head><LINK rel='stylesheet' href=\(self.stylesheet ?? " ")><meta charset='utf-8'><title>\(fileName)</title><meta name='viewport' content='width=device-width,initial-scale=1'></head><body><h1>\(fileName)</h1>\(new_result)</body></html>";
-                                if (self.output != nil) {
-                                    let filePath = FileManager.default.currentDirectoryPath + "/\(self.output ?? " ")/\(fileName).html"
-                                    if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
-                                    {
-                                        try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
-                                        print("File Created at \(filePath)")
-                                    }
-                                    else{
-                                        print("Error in writing data! Please create folder in current directory");
-                                    }
-                                }
-                                else{
-                                    let filePath = FileManager.default.currentDirectoryPath + "/Dist/\(fileName).html"
-                                    if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
-                                    {
-                                        try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
-                                        print("File Created at \(filePath)")
-                                    }
-                                    else{
-                                        print("Error in writing data! Please create folder in current directory");
-                                    }
-                                }
-                            }
-                            catch {print("Error in reading folder. Please check name and location of folder.                                           *** Folder must be in current directory ***")}
-                        }
+                    else{
+                        print("Error in writing data! Please create folder in current directory");
                     }
                 }
             }
+            catch {print("Error in reading file. Please check name and extension of file.*** File must be in current directory ***")}
+        }
+        func readAndWriteFolder(){
+            let url = URL(fileURLWithPath: FileManager.default.currentDirectoryPath + "/\(self.input)/")
+            var files = [URL]()
+            //Searching all text files in desktop directory
+            if let enumerator = FileManager.default.enumerator(at: url, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
+                for case let fileURL as URL in enumerator {
+                    do {
+                        let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
+                        if fileAttributes.isRegularFile! {
+                            files.append(fileURL)
+                        }
+                    }
+                    catch { print(error, fileURL) }
+                }
+            }
+            //Determining number of files and creating loop
+            for i in 0..<files.count {
+                let fileName = files[i].deletingPathExtension().lastPathComponent
+                if (files[i].lastPathComponent.hasSuffix(".txt")){
+                    do {
+                        //Reading & Copying the data
+                        //!!!!!!!!!!!!!!!!
+                        var data = try String(contentsOf: files[i], encoding: .utf8)
+                        data = data.replacingOccurrences(of: "\r?\n\r?\n\r?\n", with: "<p>", options: .regularExpression)
+                        data = data.replacingOccurrences(of: "\r?\n\r?\n", with: "</p><p>", options: .regularExpression)
+                        data = data.replacingOccurrences(of: "\(fileName)", with: "", options: .regularExpression)
+                        
+                        //Writing the data
+                        //!!!!!!!!!!!!!!!!
+                        let storedText =  "<!doctype html><html lang =\(self.lang ?? "en-CA")><head><LINK rel='stylesheet' href=\(self.stylesheet ?? " ")><meta charset='utf-8'><title>\(fileName)</title><meta name='viewport' content='width=device-width,initial-scale=1'></head><body><h1>\(fileName)</h1>\(data)</body></html>";
+                        
+                        //if output path is mentioned
+                        if (self.output != nil) {
+                            let filePath = FileManager.default.currentDirectoryPath + "/\(self.output ?? " ")/\(fileName).html"
+                            if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
+                            {
+                                try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
+                                print("File Created at \(filePath)")
+                            }
+                            else
+                            {
+                                print("Error in writing data! Please create folder in current directory");
+                            }
+                        }
+                        //if output path is not mentioned
+                        else{
+                            let filePath = FileManager.default.currentDirectoryPath + "/Dist/\(fileName).html"
+                            if (FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil))
+                            {
+                                try storedText.write(toFile: filePath, atomically: false, encoding: .utf8)
+                                print("File Created at \(filePath)")
+                            }
+                            else{
+                                print("Error in writing data! Please create folder in current directory");
+                            }
+                        }
+                    }
+                    catch {print("Error in reading folder. Please check name and location of folder.*** Folder must be in current directory ***")}
+                }
+            }
+        }
     }
 }
-
 Command.Main.main()
