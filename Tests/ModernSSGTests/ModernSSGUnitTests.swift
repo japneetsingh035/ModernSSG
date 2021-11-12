a0715d263860ae7effdd0737132e90a441ed489b
Japneet Singh <japneetsingh03.js@gmail.com>
2021-11-12 13:24:01 -0500

    Added ModernSSGUnitTesting

---
+++ Tests/ModernSSGTests/ModernSSGUnitTests.swift
@@ -0,0 +1,34 @@
+//
+//  ModernSSGUnitTests.swift
+//  ModernSSGTests
+//
+//  Created by Japneet Kalra on 2021-11-12.
+//
+
+import XCTest
+import ArgumentParser
+class ModernSSGUnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
